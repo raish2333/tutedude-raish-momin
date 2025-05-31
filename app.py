fe308aaff477918965c37a380f0e831f2adae064
raish2333 <raish.momin@cognyx.ai>
2025-05-31 13:31:54 +0530

    add flask and mongo db apis

--- app.py
+++ app.py
@@ -1,40 +1,45 @@
-# score = int(input("Enter your score: "))
-# if score >= 90:
-#     print("Grade: A")
-# elif score >= 80:
-#     print("Grade: B")
-# elif score >= 70:
-#     print("Grade: C")
-# elif score >= 60:
-#     print("Grade: D")
-# else:
-#     print("Grade: F")
+from flask import Flask, jsonify, request, redirect, render_template
+from pymongo import MongoClient
+import json
+from pymongo.mongo_client import MongoClient
+from dotenv import load_dotenv
+import os
 
-# grades = {}
-
-# while True:
-#     action = input("Choose an action - Add, Update, Print, or Exit: ").lower()
-#     if action == "add":
-#         name = input("Enter student name: ")
-#         grade = input("Enter grade: ")
-#         grades[name] = grade
-#     elif action == "update":
-#         name = input("Enter student name to update: ")
-#         if name in grades:
-#             grade = input("Enter new grade: ")
-#             grades[name] = grade
-#         else:
-#             print("Student not found.")
-#     elif action == "print":
-#         for student, grade in grades.items():
-#             print(f"{student}: {grade}")
-#     elif action == "exit":
-#         break
+app = Flask(__name__)
 
 
-# with open("output.txt", "w") as file:
-#     file.write("Hello, this is a test.\nThis content is written to a file.")
+@app.route('/api')
+def get_data():
+    with open('data.json') as f:
+        data = json.load(f)
+    return jsonify(data)
 
-with open("output.txt", "r") as file:
-    content = file.read()
-    print(content)
+
+
+MONGO_URI = os.getenv('MONGO_URI')
+
+# MongoDB Atlas setup
+client = MongoClient(MONGO_URI)  
+db = client['test']
+collection = db['flask-tutorial']
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    error = None
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            email = request.form['email']
+            collection.insert_one({'name': name, 'email': email})
+            return redirect('/success')
+        except Exception as e:
+            error = str(e)
+    return render_template('form.html', error=error)
+
+@app.route('/success')
+def success():
+    return 'Data submitted successfully'
+
+if __name__ == '__main__':
+    app.run(debug=True)
